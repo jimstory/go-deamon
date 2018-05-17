a62fd2e0e5c29c51882b920759c93bd681963574
步迎飞 <buyf@2345.com>
2018-05-17 15:39:36 +0800

    开启10 个worker，程序不结束，worker 一直运行

--- client.go
+++ client.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"sync"
+	"github.com/go-redis/redis"
+	"strconv"
 )
 
 func in_array(str string, strArr []string) bool {
@@ -14,11 +16,53 @@ func in_array(str string, strArr []string) bool {
 	return false
 }
 
-func main()  {
-	argswithPro := os.Args
-	commands := [] string{"status","start","stop","restart"}
-	argswithoutPro := argswithPro[1:]
-	if !in_array(argswithoutPro[0], commands) {
+//defer redisConn.Close()
+var waitGroup sync.WaitGroup
 
+func worker(i int,queueChan chan string,quit bool)  {
+	waitGroup.Add(1)
+	go func(i int) {
+		// 如果程序不结束，一直执行
+		for !quit {
+			fmt.Println("worker id："+ strconv.Itoa(i) + " queue value :" + <- queueChan)
+		}
+		waitGroup.Done()
+	}(i)
+}
+
+func main()  {
+
+	// 队列信道
+	queueChan := make(chan string, 10)
+
+	//// 是否结束程序
+	quit := false
+
+	// 建立redis 链接
+	redisConn := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	// 生成worker 协程
+	for i:= 0;i<10;i++ {
+		worker(i,queueChan,quit)
 	}
+
+	// 从redis 队列取值，放入chan
+	go func() {
+		for {
+			val, err := redisConn.LPop("queue").Result()
+			if err != nil {
+				if (err == redis.Nil) {
+					//fmt.Println("queue is empty")
+				} else {
+					panic(err.Error())
+				}
+			} else {
+				queueChan <- val
+			}
+		}
+	}()
+	waitGroup.Wait()
 }
